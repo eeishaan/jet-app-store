80b939fc9b179b98210fc64a2c272b9beecf1c59
Ishaan Kumar <ishaank@juniper.net>
2016-10-21 17:15:46 +0530

    changed add route method to grpc

--- Traffic-Management-App/TrafficManagement.py
+++ Traffic-Management-App/TrafficManagement.py
@@ -3,16 +3,19 @@
 import sys, os, re, glob, datetime, time
 from jnpr.jet.JetHandler import *
 import smtplib
+import grpc
+import rib_service_pb2 as Route
+import authentication_service_pb2
+import prpd_common_pb2
+# sender = 'JET-Router-R1@juniper.net'
+# receivers = ['gvenkata@juniper.net']
 
-sender = 'JET-Router-R1@juniper.net'
-receivers = ['gvenkata@juniper.net']
-
-R1 = 'r1k'
+R1 = '10.221.130.97'
 R1_IFL_SNMP_INDEX = '518'
 DIP = '1.1.1.1/32'
 NIP = '10.1.1.2'
-APP_USER = 'user1'
-APP_PASSWORD = 'password1'
+APP_USER = 'regress'
+APP_PASSWORD = 'MaRtInI'
 
 DEFAULT_ROUTE_NEXTHOP_IP = NIP
 DEFAULT_ROUTE_PREFERENCE_LIST = [5,15]
@@ -22,8 +25,15 @@ DEFAULT_ROUTE_FAMILY_TYPE = FamilyType.af_unspec
 DEFAULT_ROUTE_NEXTHOP_TYPE = None
 DEFAULT_ROUTE_PREFIX_AND_LENGTH = '1.1.1.1/32'
 DEFAULT_ROUTE_GET_APP_ID = '0'
-DEFAULT_ROUTE_GET_TABLE_NAME = '0'
+DEFAULT_ROUTE_GET_TABLE_NAME = 'inet6.0'
 DEFAULT_ROUTE_GET_PREFIX = '1.1.1.1'
+
+channel = grpc.insecure_channel('10.221.130.97:32767')
+sec_stub = authentication_service_pb2.LoginStub(channel)
+cred = authentication_service_pb2.LoginRequest(user_name='regress',password='MaRtInI',client_id='1211114')
+res = sec_stub.LoginCheck(cred)
+print "Authentication "+('success' if res else 'failure')
+route_stub = Route.RibStub(channel)
 
 def on_message(message):
     print("----------------------------------------EVENT RECEIVED-------------------------------------------")
@@ -43,34 +53,56 @@ def on_message(message):
         print ("\n>>>>>>>>>>>>>>Primary Path input traffic rate is above threshold value<<<<<<<<<<<<<<<<<<<")
 
         # Add route as IFL input rate is above threshold
-        nexthop_list = []
-        n_list = NexthopInfo(nexthop_address=GatewayAddress(nexthop_ip=DEFAULT_ROUTE_NEXTHOP_IP))
-        nexthop_list.append(n_list)
-        preference = DEFAULT_ROUTE_PREFERENCE_LIST
-        metric = DEFAULT_ROUTE_METRIC_LIST
-        result = R1_route_handle.RouteAdd(RouteConfig(
-                                               DEFAULT_ROUTE_APP_ID,
-                                               DEFAULT_ROUTE_FAMILY_TYPE,
-                                               DEFAULT_ROUTE_PREFIX_AND_LENGTH,
-					                           DEFAULT_ROUTE_NEXTHOP_TYPE,
-                                               nexthop_list,
-                                               None,None,preference,metric,0,None))
-        if result.err_code is 0:
+        # nexthop_list = []
+        # n_list = NexthopInfo(nexthop_address=GatewayAddress(nexthop_ip=DEFAULT_ROUTE_NEXTHOP_IP))
+        # nexthop_list.append(n_list)
+        # preference = DEFAULT_ROUTE_PREFERENCE_LIST
+        # metric = DEFAULT_ROUTE_METRIC_LIST
+
+        # result = R1_route_handle.RouteAdd(RouteConfig(
+        #                                        DEFAULT_ROUTE_APP_ID,
+        #                                        DEFAULT_ROUTE_FAMILY_TYPE,
+        #                                        DEFAULT_ROUTE_PREFIX_AND_LENGTH,
+		# 			                           DEFAULT_ROUTE_NEXTHOP_TYPE,
+        #                                        nexthop_list,
+        #                                        None,None,preference,metric,0,None))
+        
+        netaddr = prpd_common_pb2.NetworkAddress(inet=jnx_addr_pb2.IpAddress(addr_string=DEFAULT_ROUTE_GET_PREFIX))
+        rttablename = prpd_common_pb2.RouteTableName(name=DEFAULT_ROUTE_GET_TABLE_NAME)
+        routeTable = prpd_common_pb2.RouteTable(rtt_name=rttablename)
+        routematchFields = Route.RouteMatchFields(dest_prefix=netaddr,dest_prefix_len=32,table=routeTable)
+
+        routeAddr = prpd_common_pb2.NetworkAddress(inet=jnx_addr_pb2.IpAddress(addr_string=DEFAULT_ROUTE_NEXTHOP_IP))
+        gateway_list = Route.RouteGateway(gateway_address=routeAddr)
+        nexthop_list = Route.RouteNexthop(gateways=gateway_list)
+
+        route_entry = Route.RouteEntry(key=routematchFields, nexthop=nexthop_list)
+        route_request = Route.RouteUpdateRequest(routes = route_entry)
+        result = route_stub.RouteAdd(route_request)
+
+        if result.status is Route.RouteOperStatus:
             route_present = True
             print "Added static route directly into control plane"
             print "Traffic to destination subnets routed via Secondary path"
             print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
             print '\n#############################Verifying RIB#######################################'
             result = R1_route_handle.RouteGet(DEFAULT_ROUTE_GET_APP_ID, DEFAULT_ROUTE_GET_TABLE_NAME, DEFAULT_ROUTE_GET_PREFIX)
+
+            netaddr = prpd_common_pb2.NetworkAddress(inet=jnx_addr_pb2.IpAddress(addr_string="128.221.130.97"))
+            rttablename = prpd_common_pb2.RouteTableName(name=DEFAULT_ROUTE_GET_TABLE_NAME)
+            routeTable = prpd_common_pb2.RouteTable(rtt_name=rttablename)
+            routematchFields = Route.RouteMatchFields(dest_prefix=netaddr,dest_prefix_len=0,table=routeTable)
+            rrequest = Route.RouteGetRequest(key=routematchFields)
+            result = route_stub.RouteGet(rrequest)
             print result
             for routes in result.routes:
-                for hops in routes.nexthops:
-                    print 'Next Hop Ips are:',hops.nexthop_address.nexthop_ip
-                    if hops.nexthop_address.nexthop_ip == NIP:
+                for hops in routes.nexthop:
+                    print 'Next Hop Ips are:',hops.gateways.gateway_address.inet.addr_string
+                    if hops.gateways.gateway_address.inet.addr_string == NIP:
                         print "Route add API injected route successfully"
             print '##################################################################################'
-            mail (message = """Subject: JET Notification:Traffic rate is above threshold on primary path.
-            Body: JET App rerouted traffic via secondary path""")
+            # mail (message = """Subject: JET Notification:Traffic rate is above threshold on primary path.
+            # Body: JET App rerouted traffic via secondary path""")
         else:
             print "V4RouteAdd service API activation failed \n"
             route_present = False
